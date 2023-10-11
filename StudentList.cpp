6e4ecd6cf4be3273398e70d8a1e0e20eb0c65385
U-BSD\453476 <453476@SHS-H4W2NW3.beaverton.k12.or.us>
2023-10-11 09:37:53 -0700

    first commit

--- StudentList.cpp
+++ StudentList.cpp
@@ -2,12 +2,9 @@
 #include<cstring>
 #include<vector>
 #include<iterator>
+#include<iomanip>
 
 using namespace std;
-
-void add(char firstName[20], char lastName[20], int id, float gpa, struct Student &stu, vector<Student> &studentList);
-void print(vector<Student> &studentList);
-void deleteStudent();
 
 struct Student {
   char firstName[20];
@@ -16,49 +13,69 @@ struct Student {
   float gpa;
 };
 
+void add(vector<Student> &studentList);
+void print(vector<Student> &studentList);
+void deleteStudent(vector<Student> &studentList);
+
+
+
 int main () {
-  int input = 0;
-  int studentCounter = 0;
+  int studentCount = 0;
+  bool stillActive = true;
   vector<Student> studentList;
-  cout << "please enter add, remove, or print" << endl;
-  cin >> input;
-  if (input == 0) {
-    char firstName[20];
-    char lastName[20];
-    int id = 0;
-    float gpa = 0;
-    cout << "First Name:" << endl;
-    cin >> firstName;
-    Student valueOf(firstName);
-    //Student &firstName;
-    cout << "Enter a last name:" << endl;
-    cin >> lastName;
-    cout << "id number:" << endl;
-    cin >> id;
-    cout << "GPA:" << endl;
-    cin >> gpa;
-    add(firstName, lastName, id, gpa, strcpy(Student, firstName), studentList);
-    //print(studentList);
+  while(stillActive == true) {
+    cout << "Please enter add print delete or quit: " << endl;
+    char input[20];
+    cin >> input;
+  if (strcmp(input, "add") == 0) {
+    add(studentList);
+    studentCount++;
+  }
+  if (strcmp(input, "print") == 0) {
+    print(studentList);
+  }
+  if (strcmp(input, "delete") == 0) {
+    deleteStudent(studentList);
+  }
+  if (strcmp(input, "quit") == 0) {
+    stillActive = false;
+  }
   }
 
   return 0;
 }
 
-void add(char firstName[20], char lastName[20], int id, float gpa, struct Student &stu, vector<Student> &studentList) {
-  strcpy(firstName , stu.firstName);
-  strcpy(lastName , stu.lastName);
-  stu.idNum = id;
-  stu.gpa = gpa;
-  studentList.push_back(stu);
+void add(vector<Student> &studentList) {
+  Student *stu = new Student;
+  cout << "Enter a first name: " << endl;
+  cin >> stu -> firstName;
+  cout << "Enter a last name: " << endl;
+  cin >>  stu -> lastName;
+  cout << "Enter a ID number: " << endl;
+  cin >> stu -> idNum;
+  cout << "Enter a GPA: " << endl;
+  cin >> stu -> gpa;
+  studentList.push_back(*stu);
 }
 
-void print(vector<Student> studentList) {
+void print(vector<Student> &studentList) {
   vector<Student>:: iterator itr;
   for(itr = studentList.begin(); itr < studentList.end(); itr++) {
-    //cout << Student.name << endl;
+    cout << "First Name: " << (*itr).firstName << " Last Name: " << (*itr).lastName << " ID Number : " << (*itr).idNum << " GPA: " << setprecision(3) << (*itr).gpa << endl;
   }
 }
 
-void deleteStudent () {
+void deleteStudent (vector<Student> &studentList) {
+  char input[20];
+  cout << "please enter the first name of the student you wish to delete: " << endl;
+  cin >> input;
+  vector<Student>:: iterator itr;
+  for(itr = studentList.begin(); itr < studentList.end(); itr++) {
+    if(strcmp(input, (*itr).firstName) == 0) {
+      delete -> itr;
+      }
+  }
 
 }
+
+
